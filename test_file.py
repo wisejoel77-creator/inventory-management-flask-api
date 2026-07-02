b0866f8c43e96444bb80689a58500111580c8f86
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-02 22:54:07 +0300

    add @pytest.fixture decorator

--- test_file.py
+++ test_file.py
@@ -1,3 +1,11 @@
+#test to check if the inventory route is working
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.testing = True
+    return app.test_client()
 
 def test_get_inventory(client):
     response = client.get("/inventory")
