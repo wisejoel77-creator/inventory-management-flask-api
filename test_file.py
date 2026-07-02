eb90ac4bdc1444db1b29767a66b9f6c022cf98d3
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-02 23:00:54 +0300

    Add comments to my test_file.py file

--- test_file.py
+++ test_file.py
@@ -2,12 +2,16 @@
 import pytest
 from app import app
 
+#Creates a test client for the Flask app.
+#This allows us to simulate API requests without running the server.
 @pytest.fixture
 def client():
     app.testing = True
     return app.test_client()
 
+#test to get inventory items
 def test_get_inventory(client):
     response = client.get("/inventory")
 
     assert response.status_code == 200
+    assert isinstance(response.json, list)  # Check if the response is a list
